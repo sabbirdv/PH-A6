3c1622057f8e339c5913c91f6401e55000195c15
sabbirdv <sabbirhasandv@gmail.com>
2026-09-26 13:47:36 +0600

    some fix'

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-black">
-            <span className="loading loading-spinner loading-lg bg-white w-10"></span>
+            <span className="loading loading-spinner loading-lg text-[#C2F800] w-12"></span>
         </div>
     );
 };
